6a856bc6d523f9cda9493bb70d7e7c7884a2e51b
Anna Schneider <anna@bunky.(none)>
2013-04-08 16:25:48 -0700

    set up package directories and files

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+    name='ShapeMatchLib',
+    version='0.1.0',
+    author='Anna Schneider',
+    author_email='annarschneider@gmail.com',
+    packages=['shapematchlib'],
+    package_dir={'shapematchlib': 'src'},
+   # scripts=['bin/script.py'],
+    #package_data={'mypkg': ['data/*.dat']},
+    url='https://github.com/aschn/shapematchlib',
+    license='LICENSE.txt',
+    description='A library for detecting and classifying local order in spatial data.',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "matplotlib >= 1.1.1",
+        "numpy >= 1.6.1",
+        "scipy >= 0.9.0",
+        "sklearn >= 0.12.1",
+    ],
+)
