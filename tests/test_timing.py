0daf35ce54357929a3177a513ff83257baae2630
Anna Schneider <annarschneider@gmail.com>
2013-05-31 10:03:11 -0700

    add profiling tests

---
+++ tests/test_timing.py
@@ -0,0 +1,24 @@
+"""
+@package test_picolo
+@author Anna Schneider
+@version 0.1
+@brief Tests timing for picolo.matcher
+"""
+
+import nose.tools
+
+from picolo.matcher import Matcher
+
+class TestMatcherTiming:
+    
+    def setup(self):
+        self.matcher_uc = Matcher('tests/data/sample_config.xy',
+                                    name='tester',
+                                    xmlname='tests/data/sample_db_uc.xml',
+                                    imname='tests/data/sample_mask.tif',
+                                    lx=500, ly=500)
+        
+   # @nose.tools.timed(2.8)
+    def test_set_features_timing(self):
+        self.matcher_uc.set_features('test')
+
