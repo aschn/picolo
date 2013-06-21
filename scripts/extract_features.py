039e642c88d069845d90516b14365784c478b9fa
Anna Schneider <annarschneider@gmail.com>
2013-06-21 13:21:35 -0700

    add training flag to extract_features

--- scripts/extract_features.py
+++ scripts/extract_features.py
@@ -24,6 +24,8 @@ parser.add_argument('filename', type=str, help='path to xy coord file')
 parser.add_argument('shape', type=str, help='type of features to extract',
                     choices=['UnitCell', 'Fourier', 'Zernike'])
 parser.add_argument('dist', type=float, help='distance cutoff to neighbors')
+parser.add_argument('--train', action='store_true',
+                    help='include flag to only get features for prespecified training rows')
 args = parser.parse_args()
 
 # set up file paths
@@ -31,7 +33,8 @@ rootname, ext = path.splitext(args.filename)
 dirname = path.dirname(args.filename)
 
 # set up matcher
-matcher = picolo.Matcher(args.filename, delim=' ', name=rootname)
+matcher = picolo.Matcher(args.filename, delim=' ', name=rootname,
+                         trainingcol=2)
 
 # create and add default shape of correct type
 shape = shape_factory_from_values(args.shape,
@@ -43,14 +46,18 @@ matcher.shapes.add('test', shape)
 features = matcher.feature_matrix('test')
 
 # open csv writer
-outfile = path.join(dirname, '%s_features.dat' % args.shape)
+outfile = '%s_%s_features.dat' % (rootname, args.shape)
 writer = csv.writer(open(outfile, 'w'), delimiter=' ')
                     
 # write header
 writer.writerow(['id'] + shape.get_components())
 
 # loop over particle ids
-for ip in range(matcher.config.N):
+if args.train:
+    inds = matcher.training_ids
+else:
+    inds = range(matcher.config.N) 
+for ip in inds:
     
     # only write features for particles with valid shapes
     if matcher.get_features('test', ip).get('is_valid'):
